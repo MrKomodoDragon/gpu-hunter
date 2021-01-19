e0fe83482965b9534d811434b4aeacaaab57a78b
MrKomodoDragon <svrchn921@outlook.com>
2021-01-19 12:30:44 -0800

    Added separate config file

---
+++ config.py
@@ -0,0 +1,12 @@
+# Email Variables
+smtp_server = 'smtp.office365.com'
+sender_email = 'username@gmail.com'
+receiver_email = 'username@gmail.com'
+password = 'password123'
+# Website Things
+user_agent = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36 Edge/87.0.664.66'}
+product_urls = ['https://www.bestbuy.com/site/apple-airpods-pro-white/5706659.p?skuId=5706659', 'https://www.newegg.com/p/0TH-00CT-00318?Description=airpods%20pro&cm_re=airpods_pro-_-9SIAEYJD3H4300-_-Product']
+# Notification Config
+product_name = 'Apple Airpods Pro'
+webhook_url = 'url goes here'
+
--- stock.py
+++ stock.py
@@ -6,14 +6,6 @@ import os
 import smtplib
 import ssl
 port = 587  # For starttls
-smtp_server = "smtp.office365.com"
-sender_email = "my@gmail.com"
-receiver_email = "my@gmail.com"
-password = "password123"
-user_agent = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36 Edg/87.0.664.66'}
-product_urls = ['https://www.bestbuy.com/site/apple-airpods-pro-white/5706659.p?skuId=5706659' , 'https://www.newegg.com/p/0TH-00CT-00318?Description=airpods%20pro&cm_re=airpods_pro-_-9SIAEYJD3H4300-_-Product']
-keywords = 'Add to Cart'
-product_name = 'Apple Airpods Pro'
 for u in product_urls:
     subject = 'Stock Notification'
     text = 'The product {} is found in Stock. View it at {}.'.format(product_name, u)
@@ -23,7 +15,7 @@ for u in product_urls:
     context = ssl.create_default_context()
     code2 = code.encode('utf-8')
     if re.search(keywords, code2.decode('utf-8'), re.IGNORECASE):
-        webhook = DiscordWebhook(url='webhook_url_goes_here', content=f"{product_name} is in stock now at {u}")
+        webhook = DiscordWebhook(url=webhook_url, content=f"{product_name} is in stock now at {u}")
         response = webhook.execute()
         os.system(f'notify-run send \'{product_name} found in stock. Click to open the page.\' -a {u}')
         with smtplib.SMTP(smtp_server, port) as server:
